8a2b7a3cfb1ea7bda9bdf2511a5512ab7055a64b
Matthew Wright <matthew.wright5@gmail.com>
2020-05-15 15:27:13 +1000

    travis

--- lita-doubler/lib/lita/handlers/doubler.rb
+++ lita-doubler/lib/lita/handlers/doubler.rb
@@ -2,15 +2,16 @@ module Lita
   module Handlers
     class Doubler < Handler
 
-      route(
-        /^double\s+(\d+)$/i,
-        :respond_with_double,
+      route /^double\s+(\d+)$/i,
+        :double,
         command: true,
         help: { 'double N' => 'prints N + N' }
-        )
 
-      def double_number(n)
-        n + n
+      def double(response)
+        n = response.matches.first.first
+        n = Integer(n)
+
+        response.reply "#{n} + #{n} = #{n + n}"
       end
 
       Lita.register_handler(self)
--- lita-doubler/spec/lita/handlers/doubler_spec.rb
+++ lita-doubler/spec/lita/handlers/doubler_spec.rb
@@ -1,32 +1,24 @@
 require "spec_helper"
 
 describe Lita::Handlers::Doubler, lita_handler: true do
-end
+  let(:robot) { Lita::Robot.new(registry) }
 
-describe 'routing' do
-  # allow extra whitespace
-  it { is_expected.to route('Lita double 2') }
-  it { is_expected.to route('Lita double     22') }
-  # allow mixed casing.
-  it { is_expected.to route('Lita doUble 4') }
+  subject { described_class.new(robot) }
 
-  # Ignore numbers that don't look like integers.
-  it {is_expected.to_not route('Lita double two') }
-  it {is_expected.to_not route('Lita double 1e4') }
-end
+  describe ':double' do
+    # allow extra whitespace
+    it { is_expected.to route('Lita double 2') }
+    it { is_expected.to route('Lita double     22') }
+    # allow mixed casing.
+    it { is_expected.to route('Lita doUble 4') }
 
-describe ':double_number' do
-  let(:n) { rand(1.100) }
-  it 'returns double the input' do
-    actual = subject.double_number n
-    expected = n * 2
+    # Ignore numbers that don't look like integers.
+    it {is_expected.to_not route('Lita double two') }
+    it {is_expected.to_not route('Lita double 1e4') }
 
-    expect(actual).to eq(expected)
-  end
-
-  it 'doubles numbers when asked to' do
-    send_message 'Lita double 2'
-    expect(replies.last).to eq('2 + 2 = 4')
+    it 'doubles numbers when asked to' do
+      send_message 'Lita double 2'
+      expect(replies.last).to eq('2 + 2 = 4')
+    end
   end
 end
-
